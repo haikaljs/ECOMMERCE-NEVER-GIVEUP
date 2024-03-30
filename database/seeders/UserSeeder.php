530c37d58762b8097b2920a1769b6a7509e8a7b6
haikaljs <haikaljavascript@gmail.com>
2024-03-30 18:32:53 +0800

    Multi Auth - User migration, userSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'vendor',
+                'status' => 'active'
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('password'),
+                'role' => 'user',
+                'status' => 'active'
+            ],
+        ]);
+    }
+}
