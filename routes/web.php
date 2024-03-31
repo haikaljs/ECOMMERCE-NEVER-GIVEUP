ad5d309b998f9cdbf55b8da4fa20f76f1ff737fa
haikaljs <haikaljavascript@gmail.com>
2024-03-31 15:34:56 +0800

    Multi auth - Display dashboard for admin, user

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,5 +19,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->middleware(['auth'])->name('admin.dashboard');
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware(['auth'])->name('vendor.dashboard');
 
 require __DIR__.'/auth.php';
