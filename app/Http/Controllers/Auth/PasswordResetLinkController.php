cc073c44bbce1bf40de75470c1c0f42417edc0d9
haikaljs <haikaljavascript@gmail.com>
2024-04-02 05:19:07 +0800

    Frontend setup - Forget password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -36,6 +36,8 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        toastr('Password reset link has been send to your mail.');
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
