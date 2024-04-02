1ededde1424d339238e6529385705f1cd1e9855c
haikaljs <haikaljavascript@gmail.com>
2024-04-02 14:38:48 +0800

    Frontend setup - user dashbaord page

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\ProfileController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('frontend.dashboard.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
