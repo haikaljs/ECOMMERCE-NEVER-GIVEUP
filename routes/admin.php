c6761a4d9f653003abb07792cea4dae71d548d2c
haikaljs <haikaljavascript@gmail.com>
2024-04-02 20:31:47 +0800

    Admin - Slider page

--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,21 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
-use App\Http\Controllers\backend\ProfileController;
-
+use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
 // dashboard routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
+// profile routes
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+
+// slider routes
+Route::resource('slider', SliderController::class);
+
 
 
 
