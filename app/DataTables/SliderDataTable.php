2989f29c50eaaab09e010c9d8744a7394f24ba49
haikaljs <haikaljavascript@gmail.com>
2024-04-03 08:48:38 +0800

    Admin panel - Slider - show data in index page

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -22,7 +22,16 @@ class SliderDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'slider.action')
+            ->addColumn('action', function($query){
+                $editBtn = "<a href='".route('admin.slider.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+                $deleteBtn = "<a href='".route('admin.slider.destroy', $query->id)."' class='btn btn-danger ml-2'><i class='far fa-trash-alt'></i></a>";
+
+                return $editBtn.$deleteBtn;
+            })
+            ->addColumn('banner', function($query){
+                return $img = "<img width='100px' src='".asset($query->banner)."'></img>";
+            })
+            ->rawColumns(['banner', 'action'])
             ->setRowId('id');
     }
 
@@ -62,15 +71,16 @@ class SliderDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            Column::make('id')->width(60),
+            Column::make('banner')->width(200),
+            Column::make('title')->width(200),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
                   ->width(60)
                   ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+
+
         ];
     }
 
